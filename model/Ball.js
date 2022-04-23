bac64fa51a98e612b157156e57a798493d9416f3
Juan Pablo Velez <juan.velez993@gmail.com>
2022-04-23 15:29:23 -0500

    ADD---physics of collisions

--- model/Ball.js
+++ model/Ball.js
@@ -9,11 +9,39 @@ class Ball {
         this.speedX = 3;
         this.speedY = 0;
         this.direction = 1;
+        this.bounce_angle = 0;
+        this.max_bounce_angle = Math.PI / 12;
+        this.speed = 3;
 
     }
     moveBall() {
         this.x += (this.speedX * this.direction);
         this.y += (this.speedY);
+    }
+
+    get width() {
+        return this.radius * 2;
+    }
+
+    get height() {
+        return this.radius * 2;
+    }
+
+    collision(bar) {
+        var relative_intersect_y = (bar.y + (bar.height / 2)) - this.y;
+
+        var normalized_intersect_y = relative_intersect_y / (bar.height / 2);
+
+        this.bounce_angle = normalized_intersect_y * this.max_bounce_angle;
+
+        this.speed_y = this.speed * -Math.sin(this.bounce_angle);
+        this.speed_x = this.speed * Math.cos(this.bounce_angle);
+
+        if (this.x > (this.board.width / 2)) {
+            this.direction = -1;
+        } else {
+            this.direction = 1;
+        }
     }
 }
 export default Ball;
--- model/BoardView.js
+++ model/BoardView.js
@@ -42,11 +42,49 @@ class boardView {
         if (this.board.playing) {
             this.clean();
             this.drawBars();
+            this.checkCollisions();
             this.board.ball.moveBall();
         }
 
     }
 
+    checkCollisions() {
+        const bars = this.board.bars;
+        for (const bar of bars) {
+            if (this.hit(bar, this.board.ball)) {
+                this.board.ball.collision(bar);
+            }
+        }
+    }
 
+    hit(object1, object2) {
+        var hit = false;
+        if (object2.x + object2.width >= object1.x && object2.x < object1.x + object1.width) {
+
+            if (object2.y + object2.height >= object1.y && object2.y < object1.y + object1.height) {
+                hit = true;
+            }
+        }
+
+
+        if (object2.x <= object1.x && object2.x + object2.width >= object1.x + object1.width) {
+
+            if (object2.y <= object1.y && object2.y + object2.height >= object1.y + object1.height) {
+                hit = true;
+            }
+        }
+
+
+        if (object1.x <= object2.x && object1.x + object1.width >= object2.x + object2.width) {
+
+            if (object1.y <= object2.y && object1.y + object1.height >= object2.y + object2.height) {
+                hit = true;
+            }
+        }
+        return hit;
+
+
+    }
 }
+
 export default boardView;
