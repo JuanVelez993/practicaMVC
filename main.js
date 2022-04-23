e37cb4ea1ea4ca1918fc34fd7e1284b04d76778b
Juan Pablo Velez <juan.velez993@gmail.com>
2022-04-23 11:30:22 -0500

    ADD---bars to layout

--- main.js
+++ main.js
@@ -1,5 +1,6 @@
 import Board from './model/Board.js';
 import BoardView from './model/BoardView.js';
+import Bar from './model/Bar.js';
 
 
 //window.addEventListener("load", main);
@@ -7,8 +8,11 @@ import BoardView from './model/BoardView.js';
 function main() {
     const canvas = document.getElementById('canvas');
     let board = new Board(800, 400);
+    const barLeft = new Bar(20, 100, 40, 100, board);
+    const barRight = new Bar(735, 100, 40, 100, board);
     console.log(board);
     let boardView = new BoardView(canvas, board);
+    boardView.drawBars();
 
 };
 main();
--- model/BoardView.js
+++ model/BoardView.js
@@ -7,5 +7,26 @@ class boardView {
         this.context = canvas.getContext("2d");
 
     }
+
+    draw(context, element) {
+        if (element !== null && element.hasOwnProperty("kind")) {
+            console.log(element)
+            switch (element.kind) {
+                case "rectangle":
+                    context.fillRect(element.x, element.y, element.width, element.height);
+                    break;
+                default:
+                    break;
+            }
+        }
+
+    }
+
+    drawBars() {
+        const bars = this.board.bars;
+        for (const bar of bars) {
+            this.draw(this.context, bar);
+        }
+    }
 }
 export default boardView;
